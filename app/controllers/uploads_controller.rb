cb9ae4bd3fa576b5458c6dc4e91289c02b956f2c
Saheed Ajibulu <stunnafrsh007@gmail.com>
2019-03-25 13:12:10 +0100

    add upload capability using amazon s3 bucket as storage

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,13 +1,24 @@
 class UploadsController < ApplicationController
-
   def index
 
   end
 
   def new
+    @upload = Upload.new
   end
 
   def create
+    @upload = Upload.new(permitted_params)
+
+    respond_to do |format|
+      if @upload.save
+        format.html { redirect_to @upload, message: 'File upload successful' }
+        format.json { render :show, status: :created, location: @upload }
+      else
+        format.html { render :new }
+        format.json { render json: @upload.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show
@@ -23,5 +34,11 @@ class UploadsController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def permitted_params
+    params.require(:upload).permit(:title, :description, files: [])
   end
 end
--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,2 +1,3 @@
 class Upload < ApplicationRecord
+  has_many_attached :files
 end
