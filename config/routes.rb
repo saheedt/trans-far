dbc6e8211c4970fb254c3f86614614a5a3a746d9
Saheed Ajibulu <stunnafrsh007@gmail.com>
2019-03-30 23:46:07 +0100

    add download to show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :uploads
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "uploads#index"
+  root to: 'uploads#index'
 end
